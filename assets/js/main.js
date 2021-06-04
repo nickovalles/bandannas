e414eaf1bc093f4214b4f1684f1aee5fc4bd0ed1
Nilda <nick.ovalles@gmail.com>
2021-06-04 12:13:07 -0600

    <project upload>

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,7 +31,7 @@
     /* ==========================================================================
     countdown timer
     ========================================================================== */
-     jQuery('#clock').countdown('2021/10/19',function(event){
+     jQuery('#clock').countdown('2021/7/4',function(event){
       var $this=jQuery(this).html(event.strftime(''
       +'<div class="time-entry days"><span>%-D</span> Days</div> '
       +'<div class="time-entry hours"><span>%H</span> Hours</div> '
